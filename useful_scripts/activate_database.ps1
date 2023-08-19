8bed59b7580847490ec4745b3295b2e6a59a1aad
jabibo <github@binge.de>
2023-08-19 16:43:50 +0200

    Init

--- useful_scripts/activate_database.ps1
+++ useful_scripts/activate_database.ps1
@@ -1,10 +1,13 @@
+
 $env:DWH_DWS_DATABASE = ''
 $env:DWH_DWS_PASSWORD = ''
+$env:DWH_SNOWFLAKE_ACCOUNT = ''
 $env:DWH_DWS_USER = ''
-
 
 [System.Environment]::SetEnvironmentVariable('DWH_DWS_DATABASE','')
 [System.Environment]::SetEnvironmentVariable('DWH_DWS_PASSWORD', '')
 [System.Environment]::SetEnvironmentVariable('DWH_DWS_USER', '')
+[System.Environment]::SetEnvironmentVariable('DWH_SNOWFLAKE_ACCOUNT', '')
 
-dir env:DWH*
+dir env:DWH_DWS*
+dir env:DWH_SNOWFLAKE*
