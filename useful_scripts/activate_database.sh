b519048dadb300c1c66418e1c9e573dd584adc19
haas-binge <haas-binge@binge.de>
2023-08-20 13:59:52 +0200

    profiles

---
+++ useful_scripts/activate_database.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+export DWH_DATABASE=
+export DWH_PASSWORD=
+export DWH_USER=
+export DWH_SNOWFLAKE_ACCOUNT=
+echo $DWH_DATABASE
+echo $DWH_PASSWORD
+echo $DWH_USER
+echo $DWH_SNOWFLAKE_ACCOUNT
